0412c03a9c1222dcef4f82e055d834579c5f8dd3
Janek Zangenberg <zangenbergjanek@googlemail.com>
2020-12-16 13:46:57 +0100

    Document video_lag_stripper

--- src/video_lag_stripper/__init__.py
+++ src/video_lag_stripper/__init__.py
@@ -1,11 +1,26 @@
-from typing import List, Tuple
+""" Script stripping consecutive frames of insufficient rate of pixel change of passed video
+    and writing the thus resulting one to the video source path with a indicating file name 
+    extension marking it as the lag stripped file. The original video will remain untouched.
+    Video audio will not be passed along to the delagged file. 
+
+    The absolute video file path is to be passed by means of the -p or --path option during 
+    program command line invocation """
+
+
+from typing import List
 
 import cv2
 import numpy as np
 from tqdm import tqdm
 
 
-def identical_consecutive_frames_stripped_file(video_capture: cv2.VideoCapture) -> Tuple[List[np.ndarray], int]:
+def consecutive_frames_of_insufficient_rate_of_change_stripped(video_capture: cv2.VideoCapture) -> List[np.ndarray]:
+    """ Maintains merely consecutive frames of rate of pixel change
+        bigger than heuristically determined treshold 
+
+        Returns:
+            list of filtered frames """
+
     MIN_RATE_OF_CHANGE = 0.20
 
     _, last_distinct_frame = video_capture.read()
@@ -37,15 +52,30 @@ def _rate_of_change(frame1: np.ndarray, frame2: np.ndarray) -> float:
     return np.count_nonzero(diff) / diff.size
 
 
-def get_write_path(path: str, data_format='avi') -> str:
-    return path[:path.find('.')] + f' delagged.{data_format}'
+def get_write_path(path: str, video_format='avi') -> str:
+    """ Parameters:
+            path: absolute with data format suffix
+            video_format: target video format 
+
+        Returns:
+            {path_w/o_data_format_suffix} delagged.{video_format} """
+
+    return path[:path.find('.')] + f' delagged.{video_format}'
 
 
 def new_fps(original_fps: int, n_frames_original: int, n_frames_new: int) -> int:
-    return int(original_fps - n_frames_new / n_frames_original * original_fps)
+    """ Heuristically adjusts fps of delagged video to the new number of frames
+        by reducing the original fps and thus leveling the increased speed of the 
+        filtered video being a side-effect of the frame discarding
+
+        Returns:
+            original video fps reduced by what corresponds to the
+            percental change between n_frames_original and n_frames_new """
+
+    return int(original_fps - original_fps * n_frames_new / n_frames_original)
 
 
-def write_mp4(frames: List[np.ndarray], fps: int, write_path: str):
+def write_video(frames: List[np.ndarray], fps: int, write_path: str):
     height, width = frames[0].shape[:2]
     video = cv2.VideoWriter(write_path, cv2.VideoWriter_fourcc(*"XVID"), fps, (width, height))
 
@@ -76,11 +106,11 @@ if __name__ == '__main__':
     print(f'Original fps: {original_fps}')
 
     # remove consecutive frames of insufficient rate of change
-    distinct_consecutive_frames = identical_consecutive_frames_stripped_file(video_capture)
+    distinct_consecutive_frames = consecutive_frames_of_insufficient_rate_of_change_stripped(video_capture)
 
     # write processed video
     write_path = get_write_path(file_path)
-    write_mp4(frames=distinct_consecutive_frames, fps=new_fps(fps, n_frames_original, len(distinct_consecutive_frames)), write_path=write_path)
+    write_video(frames=distinct_consecutive_frames, fps=new_fps(fps, n_frames_original, len(distinct_consecutive_frames)), write_path=write_path)
 
     # display number of discarded frames
     n_discarded_frames = n_frames_original - len(distinct_consecutive_frames)
